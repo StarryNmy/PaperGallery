9af176160ff801b2e9ee82151eee8455f86d4ed4
Niu Mengyuan <102740891+StarryNmy@users.noreply.github.com>
2026-03-24 14:51:59 +0800

    fix : add base path for Github Pages
    Set the base path for the application to '/PaperGallery/'.

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig, loadEnv} from 'vite';
 export default defineConfig(({mode}) => {
   const env = loadEnv(mode, '.', '');
   return {
+    base: '/PaperGallery/', // 👈 添加这一行！注意大小写与仓库名一致
     plugins: [react(), tailwindcss()],
     define: {
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
